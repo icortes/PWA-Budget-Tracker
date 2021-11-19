91967c61a1357f4b2c66657dc2e1ae14505937a5
isa <5445946+icortes@users.noreply.github.com>
2021-11-18 18:03:35 -0800

    added functions to db.js

--- public/db.js
+++ public/db.js
@@ -1,18 +1,39 @@
-window.indexDB = window.indexDb || window.mozIndexedDB || window.webkitIndexedDb || window.msIndexedDB;
-  //make sure it is supported: 
-  if (!window.indexedDB){ 
-    alert("Not Supported");
+window.indexDB =
+  window.indexDb ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDb ||
+  window.msIndexedDB;
+//make sure it is supported:
+if (!window.indexedDB) {
+  alert('Not Supported');
 }
-  
+
+let db;
+
 const request = window.indexedDB.open('budget_db', 1);
 
-request.onupgradeneeded = event => {
+request.onupgradeneeded = (event) => {
   console.log('onupgradeneeded => executed');
-  const db = event.target.result;
+  db = event.target.result;
 
-  const budgetStore = db.createObjectStore('budget_db', { keyPath: 'budgetID' });
+  db.createObjectStore('pending', {autoIncrement: true});
+};
 
-  budgetStore.createIndex('nameIndex', 'name');
-  budgetStore.createIndex('ValueIndex', 'value');
-  budgetStore.createIndex('DateIndex', 'date');
+request.onerror = function(event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+
+request.onsuccess = (event) => {
+  db = event.request.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+function saveRecord(data) {
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const budgetStore = transaction.objectStore('pending');
+
+  budgetStore.add(data);
 }
