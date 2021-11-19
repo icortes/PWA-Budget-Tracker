54168a879702dd0db99eaf7145f414fc5c4ae99a
isa <5445946+icortes@users.noreply.github.com>
2021-11-18 16:57:02 -0800

    added onupgradenedded event to db.js

--- public/db.js
+++ public/db.js
@@ -2,4 +2,17 @@ window.indexDB = window.indexDb || window.mozIndexedDB || window.webkitIndexedDb
   //make sure it is supported: 
   if (!window.indexedDB){ 
     alert("Not Supported");
-  }
+}
+  
+const request = window.indexedDB.open('budget_db', 1);
+
+request.onupgradeneeded = event => {
+  console.log('onupgradeneeded => executed');
+  const db = event.target.result;
+
+  const budgetStore = db.createObjectStore('budget_db', { keyPath: 'budgetID' });
+
+  budgetStore.createIndex('nameIndex', 'name');
+  budgetStore.createIndex('ValueIndex', 'value');
+  budgetStore.createIndex('DateIndex', 'date');
+}
